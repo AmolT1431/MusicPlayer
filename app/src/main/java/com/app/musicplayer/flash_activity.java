31ec0767591ca70eaf828d2a7cf6c094b9f0d9d5
Amol <tambekaramol40@gmail.com>
2023-03-03 23:15:34 -0800

    flash activity created

--- app/src/main/java/com/app/musicplayer/flash_activity.java
+++ app/src/main/java/com/app/musicplayer/flash_activity.java
@@ -3,7 +3,9 @@ package com.app.musicplayer;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,6 +15,18 @@ public class flash_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flash_activity);
+        // For hide Actionbar
+        getSupportActionBar().hide();
+        //For flash Activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(flash_activity.this,MainActivity.class);
+                startActivity(i);
+
+            }
+        },1500);
+
 
     }
 }
