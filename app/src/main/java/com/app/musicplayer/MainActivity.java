3f15137974cfe446d81082eaf282da3128d0dee0
Amol <tambekaramol40@gmail.com>
2023-03-03 22:09:06 -0800

    xml change

--- app/src/main/java/com/app/musicplayer/MainActivity.java
+++ app/src/main/java/com/app/musicplayer/MainActivity.java
@@ -3,6 +3,8 @@ package com.app.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1=findViewById(R.id.b1);
+
+        b1.setOnClickListener(View);
     }
 }
