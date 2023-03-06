e2b14228887ab75f575d6381aa9ab61b8e8b9e8a
Amol <tambekaramol40@gmail.com>
2023-03-05 22:35:20 -0800

    items is created

--- app/src/main/java/com/app/musicplayer/MainActivity.java
+++ app/src/main/java/com/app/musicplayer/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_view);
         //get permission
         runtimepermission();
 
